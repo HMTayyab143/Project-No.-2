7b119ab814aa7107b402b504df2c1987539d72e9
Anton Motin <anton.motin.dev@gmail.com>
2020-06-01 20:42:43 +0300

    Initial commit, initial notes list design started

---
+++ BetterNotes/BetterNotes/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+
+namespace BetterNotes.ViewModels
+{
+    public class MainPageViewModel:BaseViewModel
+    {
+        public Command LoadNotes { get; set; }
+        public Command RemoveNote { get; set; }
+        public Command CreateNote { get; set; }
+        public Command EditNote { get; set; }
+        readonly INavigation Navigation;
+       
+        public ObservableCollection<string> Notes { get; set; }
+        public MainPageViewModel()
+        {
+            LoadNotes = new Command(() =>
+            {
+                //Load notes from the database 
+            });
+
+            RemoveNote = new Command<string>((id) =>
+            {
+                //Remove note by id
+            });
+
+            EditNote = new Command<object>((obj) =>
+            {
+                //Push a page to edit existing note using INavigation
+            });
+
+            CreateNote = new Command(() =>
+            {
+                //Push a page to create a new note
+            });
+
+        }
+
+        public MainPageViewModel(INavigation nav) : this()
+        {
+            this.Navigation = nav;
+        }
+    }
+}
---
+++ BetterNotes/BetterNotes/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using BetterNotes.ViewModels;
+
+namespace BetterNotes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel viewModel { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new MainPageViewModel(this.Navigation);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            viewModel.LoadNotes.Execute(null);
+        }
+
+        
+    }
+}
