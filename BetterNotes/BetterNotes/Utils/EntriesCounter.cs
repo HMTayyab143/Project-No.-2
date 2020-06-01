e26a00f6cbf2bdf4affa1760b5ba20ed50ad2f29
Anton Motin <anton.motin.dev@gmail.com>
2020-06-01 22:25:42 +0300

    Utility class added with a method for counting the number of new lines

---
+++ BetterNotes/BetterNotes/Utils/EntriesCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterNotes.Utils
+{
+    public static class EntriesCounter
+    {
+
+        public static int CountEntries(string text)
+        {
+            return text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
